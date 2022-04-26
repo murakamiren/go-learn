7edf0f3812c00bf7e87e1a36892dc4ee86a5f536
renmurakami <autoeyy@gmail.com>
2022-04-26 15:49:19 +0900

    prime

---
+++ prime/prime.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int = 1
+	var yakusuu int = 0
+	for i = 1; i <= 100; i++ {
+		yakusuu = 0
+		for j := 1; j <= i; j++ {
+			if i%j == 0 {
+				yakusuu++
+			}
+		}
+		if yakusuu == 2 {
+			fmt.Println(i)
+		}
+	}
+}
